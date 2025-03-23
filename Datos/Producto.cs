dee41d857ba469da21e2d587366862ec4bcc167a
Elias Rosell <yadidelias@gmail.com>
2025-03-23 13:20:29 -0700

    modelos creados

---
+++ Datos/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Producto
+    {
+        public string idProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
+        public string Descuento { get; set; }
+    }
+    
+}
---
+++ Datos/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Proveedor
+    {
+        public string idProveedor { get; set; }
+        public string Empresa { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        
+    }
+}
